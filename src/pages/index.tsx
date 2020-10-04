78b54efb0bee038e80f4f43c440894ab2318eff7
luiscostalafc <luiscostalafc@gmail.com>
2020-10-04 10:58:32 -0300

    Update configure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ import Footer from 'components/Footer'
 import JsonSchema from 'components/JsonSchema'
 
 import client from 'graphql/client'
-import GET_LANDING_PAGE from 'graphql/queries/getLandignPage'
+import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
 import { LandingPageProps } from 'types/api'
 
 const Index = ({
